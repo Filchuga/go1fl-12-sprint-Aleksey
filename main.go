6a74646aa18a188b408716635e43003cdc5e8ca2
Filchuga <filchuga@gmail.com>
2026-07-23 11:22:21 -0700

    Test DockerHub

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 )
 
 const (
-	ParcelStatusRegistered = "registered" // push
+	ParcelStatusRegistered = "registered" // Test
 	ParcelStatusSent       = "sent"
 	ParcelStatusDelivered  = "delivered"
 )
